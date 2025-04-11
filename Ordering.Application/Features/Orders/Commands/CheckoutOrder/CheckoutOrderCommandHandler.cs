980a169663066c68626406515509acf8f4a89216
Amin <AminBehrozpor1@gmail.com>
2025-04-11 16:11:54 +0330

    توسعه و تکمیل لایه Application

---
+++ Ordering.Application/Features/Orders/Commands/CheckoutOrder/CheckoutOrderCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Features.Orders.Commands.CheckoutOrder
+{
+    public class CheckoutOrderCommandHandler : IRequestHandler<CheckoutOrderCommand, int>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<CheckoutOrderCommandHandler> _logger;
+
+        public CheckoutOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper, ILogger<CheckoutOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public async Task<int> Handle(CheckoutOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderEntity = _mapper.Map<Order>(request);
+            var newOrder = await _orderRepository.AddAsync(orderEntity);
+
+            _logger.LogInformation($"order {newOrder.Id} is successfully created");
+
+            return newOrder.Id;
+        }
+    }
+}
